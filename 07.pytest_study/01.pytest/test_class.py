081bf7f1e6bfb2dfe2b7320f01ecd7b015cbf33a
liovoij <1611767852@qq.com>
2023-01-30 17:09:05 +0800

    pytest study

---
+++ 07.pytest_study/01.pytest/test_class.py
@@ -0,0 +1,11 @@
+# test_class.py
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert 't' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'hello')
+
