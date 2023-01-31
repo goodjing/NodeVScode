cae6358c82d620088b5a750d060c6a310b978f76
liovoij <1611767852@qq.com>
2023-01-31 14:43:20 +0800

    unittest

---
+++ 08.unittest_study/code/03.装饰器执行顺序.py
@@ -0,0 +1,29 @@
+import unittest
+import time
+
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('start!')
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(1)
+        print('end!')
+
+    def test01(self):
+        print('执行测试用例01')
+
+    def test03(self):
+        print('执行测试用例02')
+
+    def test02(self):
+        print('执行测试用例02')
+
+    def addtest(self):
+        print('add方法')
+
+
+if __name__ == "__main__":
+    unittest.main()
