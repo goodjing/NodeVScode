48b80dfe6035f3c4d54132b777f604284471174d
liovoij <1611767852@qq.com>
2023-02-01 16:44:04 +0800

    yield

---
+++ 07.pytest_study/code/01.pytest/pytest前后置操作/test_fixture_module.py
@@ -0,0 +1,17 @@
+import pytest
+
+@pytest.fixture(scope="module")
+def open():
+    print("打开浏览器，并且打开百度首页")
+
+def test_s1():
+    print("用例1：搜索python-1")
+
+def test_s2(open):
+    print("用例2：搜索python-2")
+
+def test_s3(open):
+    print("用例3：搜索python-3")
+
+if __name__ == "__main__":
+    pytest.main(["-s", "test_fixture_module.py"])
