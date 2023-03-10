c3f01a5a0f8affb0f7687a7e406f0728f0e99069
liovoij <1611767852@qq.com>
2023-03-10 11:38:09 +0800

    origin

---
+++ 09.python_data/code/test.py
@@ -0,0 +1,13 @@
+import sys
+import pprint
+
+pprint.pprint(sys.path)
+dog = ['haha', 'hoho', 'lolo']
+
+animal = {
+    "dog": dog,
+    "cat": "lele",
+    "fish": "yuyu"
+}
+pprint.pprint(animal)
+
