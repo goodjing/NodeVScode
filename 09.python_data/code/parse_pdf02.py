a253311baee5026e83eebc6744e19e6344e2a7e0
liovoij <1611767852@qq.com>
2023-03-24 16:43:27 +0800

    pdf02更新

--- 09.python_data/code/parse_pdf02.py
+++ 09.python_data/code/parse_pdf02.py
@@ -1,5 +1,8 @@
+import os
+
 import PyPDF2
 import pdfplumber
+from PyPDF2 import PdfFileWriter, PdfFileReader
 from openpyxl import Workbook
 
 # 1. 利用 pdfplumber 提取文字
@@ -18,3 +21,18 @@ with pdfplumber.open('../EXData/自动化办公手册.pdf') as p:
 #     workbook.save(filename="新 pdf.xlsx")
 
 # 3. 合并pdf
+# pdf_writer = PdfFileWriter()
+# for i in range(1, len(os.listdir(r"G:\6Tipdm\7python 办公自动化\concat_pdf")) + 1):
+#     print(i * 50 + 1, (i + 1) * 50)
+#     pdf_reader = PdfFileReader("G:\\6Tipdm\\7python 办公自动化\\concat_pdf\{}-{}.pdf".format(i * 50 + 1, (i + 1) * 50))
+#     for page in range(pdf_reader.getNumPages()):
+#         pdf_writer.addPage(pdf_reader.getPage(page))
+# with open("G:\\6Tipdm\\7python 办公自动化\\concat_pdf\merge.pdf", "wb") as out:
+#     pdf_writer.write(out)
+
+# 4. 拆分pdf
+# pdf_reader = PdfFileReader(r"G:\6Tipdm\7python 办公自动化\concat_pdf\时间序列.pdf")
+# for page in range(pdf_reader.getNumPages()):
+#     pdf_writer = PdfFileWriter()
+#     pdf_writer.addPage(pdf_reader.getPage(page))
+#     with open(f"G:\\6Tipdm\\7python 办公自动化\\concat_pdf\\{page}.pdf", "wb") as out: pdf_writer.write(out)
